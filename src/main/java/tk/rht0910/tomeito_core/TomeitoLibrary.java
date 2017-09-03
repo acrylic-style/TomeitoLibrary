1a15eca05e07a25e08b310ba71fe5f302a2b7587
rht0910 <rht0910@outlook.jp>
2017-09-03 23:20:37 +0900

    This is testing!

---
+++ src/main/java/tk/rht0910/tomeito_core/TomeitoLibrary.java
@@ -0,0 +1,15 @@
+package tk.rht0910.tomeito_core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TomeitoLibrary extends JavaPlugin {
+	@Override
+	public void onLoad() {
+
+	}
+
+	@Override
+	public void onEnable() {
+
+	}
+}
---
+++ src/main/java/tk/rht0910/tomeito_core/exception/DebugException.java
@@ -0,0 +1,7 @@
+package tk.rht0910.tomeito_core.exception;
+
+public class DebugException extends Exception {
+	public DebugException() {
+		super("(THIS IS NOT AN ERROR)");
+	}
+}
