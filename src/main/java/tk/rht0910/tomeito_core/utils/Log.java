f872c4a6b8e4e5446372a2cdb67d6a13375bcbc4
rht0910 <rht0910@outlook.jp>
2017-10-03 17:43:28 +0900

    o0 (No comments)

--- src/main/java/tk/rht0910/tomeito_core/utils/Log.java
+++ src/main/java/tk/rht0910/tomeito_core/utils/Log.java
@@ -9,69 +9,69 @@ import org.bukkit.Bukkit;
  *
  */
 public class Log {
-	private static String classn = Thread.currentThread().getStackTrace()[2].getClassName();
+	private static String[] classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
 
 	public Log(String className) {
-		classn = className;
+		//classn = className;
 	}
 
 	public static void info(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".")[];
 		Bukkit.getLogger().info("[" + classn + "] " + msg);
 	}
 
 	public static void warning(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().warning("[" + classn + "] " + msg);
 	}
 
 	public static void warn(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().warning("[" + classn + "] " + msg);
 	}
 
 	public static void severe(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().severe("[" + classn + "] " + msg);
 	}
 
 	public static void error(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().severe("[" + classn + "] " + msg);
 	}
 
 	public static void config(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().config("[" + classn + "] " + msg);
 	}
 
 	public static void fine(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().fine("[" + classn + "] " + msg);
 	}
 
 	public static void finer(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().finer("[" + classn + "] " + msg);
 	}
 
 	public static void finest(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().finest("[" + classn + "] " + msg);
 	}
 
 	public static void debug(String msg) {
-		//classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
-		classn = Thread.currentThread().getStackTrace()[2].getClassName();
+		classn = Thread.currentThread().getStackTrace()[2].getClassName().split(".");
+		//classn = Thread.currentThread().getStackTrace()[2].getClassName();
 		Bukkit.getLogger().info("[" + classn + "] [DEBUG] " + msg);
 	}
 }
