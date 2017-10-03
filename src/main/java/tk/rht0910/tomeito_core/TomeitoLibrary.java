5175b72298aa9efa08ff1d279bf975a927fc4ac7
rht0910 <rht0910@outlook.jp>
2017-10-03 16:25:09 +0900

    the 2nd commits.

--- src/main/java/tk/rht0910/tomeito_core/TomeitoLibrary.java
+++ src/main/java/tk/rht0910/tomeito_core/TomeitoLibrary.java
@@ -1,10 +1,11 @@
 package tk.rht0910.tomeito_core;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import tk.rht0910.tomeito_core.utils.Log;
 
-public class TomeitoLibrary extends JavaPlugin {
+public class TomeitoLibrary extends JavaPlugin implements Listener {
 	@Override
 	public void onLoad() {
 
--- src/main/java/tk/rht0910/tomeito_core/utils/Log.java
+++ src/main/java/tk/rht0910/tomeito_core/utils/Log.java
@@ -2,44 +2,50 @@ package tk.rht0910.tomeito_core.utils;
 
 import org.bukkit.Bukkit;
 
+/**
+ *
+ * @author tomeito0110
+ * Get logger in using Bukkit.
+ *
+ */
 public class Log {
 	public static void info(String msg) {
-		Bukkit.getLogger().info("[PluginManager] " + msg);
+		Bukkit.getLogger().info("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void warning(String msg) {
-		Bukkit.getLogger().warning("[PluginManager] " + msg);
+		Bukkit.getLogger().warning("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void warn(String msg) {
-		Bukkit.getLogger().warning("[PluginManager] " + msg);
+		Bukkit.getLogger().warning("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void severe(String msg) {
-		Bukkit.getLogger().severe("[PluginManager] " + msg);
+		Bukkit.getLogger().severe("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void error(String msg) {
-		Bukkit.getLogger().severe("[PluginManager] " + msg);
+		Bukkit.getLogger().severe("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void config(String msg) {
-		Bukkit.getLogger().config("[PluginManager] " + msg);
+		Bukkit.getLogger().config("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void fine(String msg) {
-		Bukkit.getLogger().fine("[PluginManager] " + msg);
+		Bukkit.getLogger().fine("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void finer(String msg) {
-		Bukkit.getLogger().finer("[PluginManager] " + msg);
+		Bukkit.getLogger().finer("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void finest(String msg) {
-		Bukkit.getLogger().finest("[PluginManager] " + msg);
+		Bukkit.getLogger().finest("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] " + msg);
 	}
 
 	public static void debug(String msg) {
-		Bukkit.getLogger().info("[PluginManager] [DEBUG] " + msg);
+		Bukkit.getLogger().info("[" + Thread.currentThread().getStackTrace()[2].getClassName() + "] [DEBUG] " + msg);
 	}
 }
