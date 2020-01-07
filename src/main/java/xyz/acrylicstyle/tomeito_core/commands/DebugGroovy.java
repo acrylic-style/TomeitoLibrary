bd345e81935ca97b2c87a62e1dad0446db027f3e
acrylic-style <me@acrylicstyle.xyz>
2020-01-07 16:45:02 +0900

    bug fix

--- src/main/java/xyz/acrylicstyle/tomeito_core/commands/DebugGroovy.java
+++ src/main/java/xyz/acrylicstyle/tomeito_core/commands/DebugGroovy.java
@@ -1,16 +1,12 @@
 package xyz.acrylicstyle.tomeito_core.commands;
 
+import groovy.util.Eval;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import java.lang.reflect.Modifier;
 
 public class DebugGroovy {
-    public static final ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("groovy");
-
     public static void run(CommandSender sender, String[] args) {
         if (!sender.hasPermission("*") || !sender.isOp()) {
             sender.sendMessage(ChatColor.RED + "Sorry but you don't have enough permission.");
@@ -25,10 +21,10 @@ public class DebugGroovy {
         for (int i = 1; i < args.length; i++) argsSB.append(args[i]);
         String argsString = argsSB.toString();
         try {
-            Object result = scriptEngine.eval(argsString);
+            Object result = Eval.me(argsString);
             sender.sendMessage(ChatColor.GREEN + "Result[" + (result != null ? Modifier.toString(result.getClass().getModifiers()) : "<?>") + "](" + (result != null ? result.getClass().getCanonicalName() : "null") + "):");
             sender.sendMessage(ChatColor.GREEN + "" + result);
-        } catch (ScriptException e) {
+        } catch (Exception e) {
             sender.sendMessage(ChatColor.RED + "An error occurred: " + e);
             for (StackTraceElement st : e.getStackTrace()) {
                 sender.sendMessage(ChatColor.RED + "    " + st.toString());
