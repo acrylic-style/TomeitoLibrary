4c8d6c44c47e5457fc80a45fff2d259a2579dd5f
acrylic-style <me@acrylicstyle.xyz>
2020-03-21 17:04:04 +0900

    bug fix

--- src/main/java/xyz/acrylicstyle/tomeito_core/subcommand/OpSubCommandExecutor.java
+++ src/main/java/xyz/acrylicstyle/tomeito_core/subcommand/OpSubCommandExecutor.java
@@ -1,20 +1,15 @@
 package xyz.acrylicstyle.tomeito_core.subcommand;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public abstract class OpSubCommandExecutor implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+public abstract class OpSubCommandExecutor implements SubCommandExecutor {
+    public void onCommand(CommandSender sender, String[] args) {
         if (!sender.isOp()) {
             sender.sendMessage(ChatColor.RED + "You don't have permission to run this command.");
-            return true;
         }
-        onCommand(sender, args);
-        return true;
+        onOpCommand(sender, args);
     }
 
-    public abstract void onCommand(CommandSender player, String[] args);
+    public abstract void onOpCommand(CommandSender sender, String[] args);
 }
