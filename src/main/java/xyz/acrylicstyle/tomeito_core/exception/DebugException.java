a36689df13a14abe0bc0246408698fc17a57a1b5
acrylic-style <me@acrylicstyle.xyz>
2020-04-09 18:00:16 +0900

    delete deprecated DebugException.java

--- src/main/java/xyz/acrylicstyle/tomeito_core/exception/DebugException.java
+++
@@ -1,9 +0,0 @@
-package xyz.acrylicstyle.tomeito_core.exception;
-
-@SuppressWarnings("unused")
-@Deprecated
-public class DebugException extends Exception {
-    public DebugException() {
-        super("(THIS IS NOT AN ERROR)");
-    }
-}
