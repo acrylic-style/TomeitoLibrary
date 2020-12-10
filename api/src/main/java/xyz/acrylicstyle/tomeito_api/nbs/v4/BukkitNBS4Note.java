37f150958bdf8d5a7b3b0511991d53e3e0235966
acrylic-style <ilyy@outlook.jp>
2020-12-11 03:00:13 +0900

    Fix pitch again

--- api/src/main/java/xyz/acrylicstyle/tomeito_api/nbs/v4/BukkitNBS4Note.java
+++ api/src/main/java/xyz/acrylicstyle/tomeito_api/nbs/v4/BukkitNBS4Note.java
@@ -23,32 +23,32 @@ public class BukkitNBS4Note extends NBS4Note implements BukkitNBSNote {
     @Override
     public float getSoundPitch() {
         byte key = super.getKey();
-        if (key == 33) return 0.0F;
-        if (key == 34) return 0.53F;
-        if (key == 35) return 0.57F;
-        if (key == 36) return 0.6F;
-        if (key == 37) return 0.65F;
-        if (key == 38) return 0.68F;
-        if (key == 39) return 0.7F;
-        if (key == 40) return 0.75F;
-        if (key == 41) return 0.8F;
-        if (key == 42) return 0.85F;
-        if (key == 43) return 0.9F;
-        if (key == 44) return 0.95F;
-        if (key == 45) return 1.0F;
-        if (key == 46) return 1.05F;
-        if (key == 47) return 1.15F;
-        if (key == 48) return 1.2F;
-        if (key == 49) return 1.3F;
-        if (key == 50) return 1.35F;
-        if (key == 51) return 1.4F;
-        if (key == 52) return 1.5F;
-        if (key == 53) return 1.6F;
-        if (key == 54) return 1.7F;
-        if (key == 55) return 1.8F;
-        if (key == 56) return 1.9F;
-        if (key == 57) return 2.0F;
-        return 0.0F;
+        if (key == 33) return 0.5F;      // F#
+        if (key == 34) return 0.529732F; // G
+        if (key == 35) return 0.561231F; // G#
+        if (key == 36) return 0.594604F; // A
+        if (key == 37) return 0.629961F; // A#
+        if (key == 38) return 0.667420F; // B
+        if (key == 39) return 0.707107F; // C
+        if (key == 40) return 0.749154F; // C#
+        if (key == 41) return 0.793701F; // D
+        if (key == 42) return 0.840896F; // D#
+        if (key == 43) return 0.890899F; // E
+        if (key == 44) return 0.943874F; // F
+        if (key == 45) return 1.0F;      // F#
+        if (key == 46) return 1.059463F; // G
+        if (key == 47) return 1.122462F; // G#
+        if (key == 48) return 1.189207F; // A
+        if (key == 49) return 1.259921F; // A#
+        if (key == 50) return 1.334840F; // B
+        if (key == 51) return 1.414214F; // C
+        if (key == 52) return 1.498307F; // C#
+        if (key == 53) return 1.587401F; // D
+        if (key == 54) return 1.681793F; // D#
+        if (key == 55) return 1.781797F; // E
+        if (key == 56) return 1.887749F; // F
+        if (key == 57) return 2.0F;      // F#
+        return 0.0F;                     // out of range
     }
 
     /**
@@ -75,6 +75,7 @@ public class BukkitNBS4Note extends NBS4Note implements BukkitNBSNote {
         if (instrument == 13) return Sound.BLOCK_NOTE_BIT;
         if (instrument == 14) return Sound.BLOCK_NOTE_BANJO;
         if (instrument == 15) return Sound.BLOCK_NOTE_PLING;
+        // TODO: Support custom instruments (begin from 16)
         throw new IndexOutOfBoundsException("Instrument is out of range, unsupported version?");
     }
 }
